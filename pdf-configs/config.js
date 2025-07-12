1df6c8be8d81750a22451f8df6773d899c2b2ee2
yuya03 <yuya.yagai@uzabase.com>
2025-07-12 09:39:28 +0900

    update 20250710

--- pdf-configs/config.js
+++ pdf-configs/config.js
@@ -4,11 +4,16 @@ module.exports = {
     marked_options: {
       headerIds: false,
       smartypants: true,
+      html: true,
+      breaks: false,
     },
     pdf_options: {
       "format": "A4",
       "margin": "30mm 20mm",
       "printBackground": true,
+      "displayHeaderFooter": true,
+      "waitForNetworkIdle": true,
+      "timeout": 30000,
       "headerTemplate": "<style>\n  section {\n    margin: 0 auto;\n    font-size: 9px;\n  }\n</style>",
       "footerTemplate": "<section>\n  <div>\n    <span class=\"pageNumber\"></span>\n    / <span class=\"totalPages\"></span>\n  </div>\n</section>"
     },
